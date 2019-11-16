d6ba7868c654f51b8d32b1e40f4e7c89beb2e797
Gabriel Esteban <yufuven@gmail.com>
2019-11-16 22:03:25 +0200

    Removed js and map of popup

--- src/widget/components/popup/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import "./popup.css";
-export class Hello extends React.Component {
-    render() {
-        return (React.createElement("div", { className: "popup-padded" },
-            React.createElement("h1", null, "Hello React")));
-    }
-}
-//# sourceMappingURL=index.js.map
