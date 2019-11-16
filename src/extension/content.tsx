f21ae1d07c8aa00ea52eddc05c1650f3aaf6215a
Gabriel Esteban <yufuven@gmail.com>
2019-11-16 22:29:21 +0200

    asdasd

--- src/extension/content.tsx
+++ src/extension/content.tsx
@@ -2,15 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Overlay } from "../overlay/Overlay";
 
-chrome.runtime.sendMessage({}, (response) => {
-  var checkReady = setInterval(() => {
-    if (document.readyState === "complete") {
-      clearInterval(checkReady);
-      console.log("We're in the injected content script!");
-    }
-  });
-});
-
 const element = document.createElement("div");
 element.id = "extroot";
 
