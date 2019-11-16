c832191dc38a70897712bdc5cfbc9fe013052abb
Kylar13 <ramonpans13@gmail.com>
2019-11-16 14:03:33 +0200

    Revert "Better file structure"
    This reverts commit a7718e89a185ccdec3aa134d9afa42cfb65d5cc4.

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,3 +1,4 @@
 import * as React from "react"
+import * as ReactDOM from "react-dom"
 
 export const Test = () => <div style={{ position: "fixed", display: "flex", top: 0, right: 0, left: 0, bottom: 0, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0,0,0,0.6)", zIndex: 9999 }}><p>Hola Carlos</p></div>;
